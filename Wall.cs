8e32038e2e3f4b9659db99ca1140eb3d90d42762
adek06 <adek06@163.com>
2017-08-05 21:39:40 +0800

    加入了人物行走的脚本

---
+++ Wall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+
+    public int hp = 2;
+    public Sprite damageSprite;
+
+    private void TakeDamage() {
+        hp -= 1;
+        GetComponent<SpriteRenderer>().sprite = damageSprite;
+        if (hp <= 0) {
+            Destroy(this.gameObject);
+        }
+    }
+}
