719cb5522e6b336932efae232a6c9f3dcccd1f9b
adek06 <adek06@163.com>
2017-08-05 21:37:02 +0800

    到现在，加入了人物行走及碰撞机

---
+++ MapManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour {
+
+    private Transform mapHolder;
+    public GameObject[] floorArray;
+    public GameObject[] outWallArray;
+    public GameObject[] wallArray;
+    public GameObject[] foodArray;
+    public GameObject[] enemyArray;
+    public GameObject exitUI;
+    private GameManager gameManager;
+    public int rows;
+    public int cols;
+
+    private List<Vector2> positionList = new List<Vector2>();
+
+    void Awake() {
+        gameManager =this.GetComponent<GameManager>();
+        InitMap();
+	}
+
+    private void InitMap(){
+        mapHolder = new GameObject("Map").transform;
+        makeFrame(rows,cols);
+        makeHinder(rows,cols);
+	}
+
+    private void makeFrame(int rows,int cols){
+        for (int x = 0; x < rows; x++)
+        {
+            for (int y = 0; y < cols; y++)
+            {
+                if (x == 0 || y == 0 || x == rows - 1 || y == cols - 1)
+                {
+                    creatMap(outWallArray,new Vector2(x,y));
+                }
+                else
+                {
+                    creatMap(floorArray,new Vector2(x,y));
+                }
+            }
+        }
+    }
+
+    private void makeHinder(int rows,int cols){
+        positionList.Clear();
+        for (int x = 2; x < rows-2 ; x++) {
+            for (int y = 2; y < cols-2 ; y++) {
+                positionList.Add(new Vector2(x, y));
+            }
+        }
+
+        int minCountWall = 0;
+        int minFood = 0;
+        int minEnemy = 0;
+        decorateHinder(minCountWall,gameManager.level*2,wallArray);
+        decorateHinder(minFood,gameManager.level,foodArray);
+        decorateHinder(minEnemy,gameManager.level/2,enemyArray);
+
+        GameObject exit = Instantiate(exitUI, new Vector2(cols - 2, rows - 2), Quaternion.identity);
+        exit.transform.SetParent(mapHolder);
+    }
+
+    private void decorateHinder(int mix,int max,GameObject[] array) {
+        int count = Random.Range(mix, max);
+        for (int i = 0; i < count; i++) {
+            int positionIndex = Random.Range(0, positionList.Count);
+            creatMap(array,positionList[positionIndex]);
+            positionList.RemoveAt(positionIndex);
+        }
+    }
+
+    private void creatMap(GameObject[] array, Vector2 pos)
+    {
+            int arrayIndex = Random.Range(0, array.Length);
+            GameObject map = Instantiate(array[arrayIndex],pos, Quaternion.identity);
+            map.transform.SetParent(mapHolder);
+    }
+
+}
---
+++ Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float smoothing = 6f;
+    public float restTime = 0.19f;
+    public float restTimer = 0;
+
+    private Vector2 targetPos = new Vector2(1, 1);
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().MovePosition(Vector2.Lerp(transform.position, targetPos, smoothing * Time.deltaTime));
+        restTimer += Time.deltaTime;
+        if (restTimer < restTime)
+        {
+            return;
+        }
+        else
+        {
+
+            float h = Input.GetAxisRaw("Horizontal");
+            float v = Input.GetAxisRaw("Vertical");
+            if (h != 0)
+            {
+                v = 0;
+            }
+            if (h != 0 || v != 0)
+            {
+                deteHinder(h, v);
+            }
+
+        }
+    }
+
+    private void deteHinder(float h, float v)
+    {
+        GetComponent<BoxCollider2D>().enabled = false;
+        RaycastHit2D hit = Physics2D.Linecast(targetPos, targetPos + new Vector2(h, v));
+        GetComponent<BoxCollider2D>().enabled = true;
+
+        if (hit.transform == null)
+        {
+            targetPos += new Vector2(h, v);
+            restTimer = 0;
+        }
+        else if (hit.collider.tag == "Wall")
+        {
+            GetComponent<Animator>().SetTrigger("Attack");
+            hit.collider.SendMessage("TakeDamage");
+            restTimer = 0;
+        }
+        else if (hit.collider.tag == "OutWall")
+        {
+            restTimer = 0;
+        }
+    }
+}
